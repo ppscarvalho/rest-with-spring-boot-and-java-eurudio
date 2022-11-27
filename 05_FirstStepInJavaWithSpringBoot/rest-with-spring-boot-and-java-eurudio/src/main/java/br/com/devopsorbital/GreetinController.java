f06c0bd9d956a1f89be8807641c8c5994d4908bb
Pedro Carvalho <ppscarvalho@gmail.com>
2022-11-27 15:55:19 -0300

    Secund Steps Rest in Java with Spring Boot 4

--- 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-eurudio/src/main/java/br/com/devopsorbital/GreetinController.java
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-eurudio/src/main/java/br/com/devopsorbital/GreetinController.java
@@ -3,7 +3,6 @@ package br.com.devopsorbital;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
